b3a904255876e6068a14bfe0e724f8c534cf8f87
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-10-29 17:23:57 +0100

    desktop layout completed

--- link-tree/src/App.js
+++ link-tree/src/App.js
@@ -4,12 +4,15 @@ import Linker from './component/links';
 import './links.css'
 import Footer from './component/footer';
 import './footer.css'
+import Socials from './component/socials';
+import './social.css'
 
 function App(){
     return(
         <div className='App'>
             <Profile />
             <Linker  />
+            <Socials />
             <Footer />
         </div>
     )
---
+++ link-tree/src/component/socials.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSlack } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+class Socials extends Component {
+    render() { 
+        return (
+            <div className="socials">
+                <ul>
+                    <li>
+                        <a href=""><FontAwesomeIcon icon={faSlack} /></a>
+                    </li>
+                    <li>
+                    <a href=""><FontAwesomeIcon icon={faGithub} /></a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default Socials;
