91fc73d1489efcf6c1e2eb28ae8f9fe96f29ea5b
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-10-29 01:50:27 +0100

    profile image done

---
+++ link-tree/src/component/profile.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() { 
+    return(
+      <React.Fragment>
+        <img src="/img/profile_img.jpg" alt="profile_image" id='profile_img' />
+      </React.Fragment>
+      
+    )
+  }
+}
+ 
+export default Profile;
--- link-tree/src/index.js
+++ link-tree/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Profile from './component/profile';
+import './profile.css'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Profile />
   </React.StrictMode>
 );
 
