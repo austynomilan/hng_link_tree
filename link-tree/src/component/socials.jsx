28abed8482797a951893f3914021b1174bb3d555
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-10-31 14:10:55 +0000

    socials updated

--- link-tree/src/component/socials.jsx
+++ link-tree/src/component/socials.jsx
@@ -9,7 +9,7 @@ class Socials extends Component {
             <div className="socials">
                 <ul>
                     <li>
-                        <a id='slack' href="https://hng9.slack.com/team/U04903CUJL9"><FontAwesomeIcon icon={faSlack} /></a>
+                        <a id='slack_' href="https://hng9.slack.com/team/U04903CUJL9"><FontAwesomeIcon icon={faSlack} /></a>
                     </li>
                     <li>
                     <a href="https://github.com/austynomilan"><FontAwesomeIcon icon={faGithub} /></a>
