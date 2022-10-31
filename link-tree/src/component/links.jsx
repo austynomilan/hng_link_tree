e373a056d4a8b36967c512c4cb67d558e604597d
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-10-31 13:31:34 +0000

    fixed the links and targets

--- link-tree/src/component/links.jsx
+++ link-tree/src/component/links.jsx
@@ -3,25 +3,23 @@ import React, { Component } from 'react';
 class Linker extends Component {
     render() { 
         return (
-        <div className='linkers'>
-            <ul>
-                <li>
+        <section className='linkers'>
+
                     <a href="https://training.zuri.team/" id='btn_zuri'>Zuri Team</a>
-                    </li>
-                <li>
-                    <a href="http://books.zuri.team" id='books' title='Handpicked books and customized guides to help you grow and advance your tech careers.'>Zuri Books</a>
-                    </li>
-                <li>
-                    <a href="https://books.zuri.team/python-for-beginners?ref_id=<Austyno>" id='book_python' title='The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.'>Python Books</a>
-                </li>
-                <li>
-                    <a href="https://background.zuri.team" id='pitch' title='With the right digital skill set and access to the global freelance marketplace, you can now offer your services online and get paid in dollars from home. Ready?'>Background Check For Codes</a>
-                    </li>
-                <li>
-                    <a href="https://books.zuri.team/design-rules" id='book_design' title='The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.'>Design Books</a>
-                    </li>
-            </ul>
-        </div>
+                
+        
+                    <a href="http://books.zuri.team" id='books' title='Handpicked books and customized guides to help you grow and advance your tech careers.' target='_blank' rel="noreferrer">Zuri Books</a>
+                    
+        
+                    <a href="https://books.zuri.team/python-for-beginners?ref_id=<Austyno>" id='book_python' title='The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.' target='_blank' rel="noreferrer">Python Books</a>
+            
+        
+                    <a href="https://background.zuri.team" id='pitch' title='With the right digital skill set and access to the global freelance marketplace, you can now offer your services online and get paid in dollars from home. Ready?' target='_blank' rel="noreferrer">Background Check For Codes</a>
+                    
+        
+                    <a href="https://books.zuri.team/design-rules" id='book_design' title='The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.' target='_blank' rel="noreferrer">Design Books</a>
+                
+        </section>
         );
     }
 }
