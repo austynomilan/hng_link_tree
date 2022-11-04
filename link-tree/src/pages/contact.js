a4adccb78b9a6b2f63172e2ec3f35c8f9fc9dddd
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-11-04 08:12:25 +0000

    Refactor: the App file was modified

---
+++ link-tree/src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./index.css";
+
+import React from 'react'
+
+constContact = () => {
+  return (
+    <div>
+      <h1>contact page</h1>
+    </div>
+  )
+}
+
+export default Contact;
