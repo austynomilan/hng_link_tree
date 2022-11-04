2933f346a460e1635b6ff060ab11cf4f426cf22f
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-11-04 10:46:50 +0000

    feature: created contact page

--- link-tree/src/index.js
+++ link-tree/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import './App.css'
+import Contact from './pages/contact';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
@@ -14,8 +15,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <App /> } />
-     </Routes>
+        <Route path='/' element={<App />} />
+      </Routes>
+      <Routes>
+        <Route path='/Contact' element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
